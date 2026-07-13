090fe722d07cdea61be5a4d3dfded137fab3bfab
Marina Muñoz <marinacerejidoo@gmail.com>
2026-07-13 19:27:21 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./styles/tailwind.css";
-import logo from "./assets/1685266102882.jfif";
+import logo from "/logo.png";
 
 function App() {
   return (
@@ -8,34 +8,23 @@ function App() {
       <div className="flex flex-col gap-2">
         <div className="mb-5 flex flex-col gap-3">
           <img src={logo} className="w-1/5 rounded-full block m-auto" />
-          <h1 className="dark:text-white">@spanishgitana</h1>
+          <h1 className="text-[#8b2020] text-lg dark:text-white mb-[-10px]">
+            @spanishgitana
+          </h1>
         </div>
-
         <a
           href="https://open.spotify.com/show/4KYtGSXTcR9dj22kuNy1Yg?si=t8pDJoLUQIKBT35ejbh_Ow&nd=1&dlsi=78633e8d365447ed"
           target="_blank"
           className="link"
         >
-          Podcast on Spotify
+          Slow & Clear Spanish - spotify
         </a>
-
         <a
           href="https://www.youtube.com/channel/UCtCqI5Q9Jyn5MtU_Oqo2e6g"
           target="_blank"
           className="link"
         >
-          YouTube
-        </a>
-
-        <a
-          href="https://slow-and-clear-spanish-pod.beehiiv.com/"
-          target="_blank"
-          className="link"
-        >
-          Newsletter{" "}
-          <span style={{ fontSize: "13px" }}>
-            (summary of each podcast episode, vocabulary list & extra content)
-          </span>
+          Slow & Clear Spanish - youtube
         </a>
 
         <a
@@ -45,7 +34,6 @@ function App() {
         >
           Master Spanish Verbs - course
         </a>
-
         <a
           href="https://spanishgitana.thinkific.com/courses/spanish-from-scratch"
           target="_blank"
@@ -53,7 +41,6 @@ function App() {
         >
           Spanish from Scratch - course
         </a>
-
         <a
           href="https://www.instagram.com/spanishgitana/"
           target="_blank"
